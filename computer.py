ba31cd9388a8962101977df16671fd41a6f5db9d
candycoatedrox <60051903+candycoatedrox@users.noreply.github.com>
2025-09-18 11:24:29 -0400

    Computer and ResaleShop classes implemented

--- computer.py
+++ computer.py
@@ -1,10 +1,52 @@
+from typing import Optional
+
 class Computer:
 
-    # What attributes will it need?
+    def __init__(self,
+                 description: str,
+                 processorType: str,
+                 hardDriveCapacity: int,
+                 memory: int,
+                 os: str,
+                 yearMade: int,
+                 price: int):
+        
+        self.desc = description
+        self.processor = processorType
+        self.hardDriveCapacity = hardDriveCapacity
+        self.memory = memory
+        self.os = os
+        self.yearMade = yearMade
+        self.price = price
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    """
+    Returns a nice-looking, readable string representation of a computer's information
+    """
+    def __str__(self):
+        return f"{self.desc}: {self.processor} processor, {self.hardDriveCapacity} GB hard drive, {self.memory} GB RAM, runs {self.os}, {self.yearMade} model, ${self.price}"
 
-    # What methods will you need?
+    def __repr__(self):
+        return f"(description: '{self.desc}', processor: '{self.processor}', hardDriveCapacity: {self.hardDriveCapacity}, memory: {self.memory}, os: '{self.os}', yearMade: {self.yearMade}, price: {self.price})"
+
+    """
+    Takes in a new price, updates the computer's price to match
+    """
+    def updatePrice(self, newPrice: int):
+        self.price = newPrice
+    
+    """
+    Updates a computer's price depending on its yearMade,
+    updates its os if a new one is given
+    """
+    def refurbish(self, newOS: Optional[str] = None):
+        if self.yearMade < 2000:
+            self.price = 0 # too old to sell, donation only
+        elif self.yearMade < 2012:
+            self.price = 250 # heavily-discounted price on machines 10+ years old
+        elif self.yearMade < 2018:
+            self.price = 550 # discounted price on machines 4-to-10 year old machines
+        else:
+            self.price = 1000 # recent stuff
+
+        if newOS is not None:
+            self.os = newOS
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,65 @@
+from computer import Computer
+
 class ResaleShop:
 
-    # What attributes will it need?
+    def __init__(self):
+        self.inventory : list = []
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __getitem__(self, i: int):
+        return self.inventory[i]
 
-    # What methods will you need?
+    """
+    Takes in all the information about a computer,
+    adds it to the inventory, returns its index in the inventory
+    """
+    def buy(self, c: Computer):
+        self.inventory.append(c)
+        return len(self.inventory) - 1
+    
+    """
+    Takes in an item index, removes the associated computer if it is the inventory, 
+    prints error message otherwise
+    """
+    def sell(self, id: int):
+        if self[id] is not None:
+            c = self.inventory.pop(id)
+            print(f'Item {id} ({c.desc}) sold!')
+        else:
+            print(f'Item {id} not found. Please select another item to sell.')
+    
+    """
+    Prints all the items in the inventory (if it isn't empty), prints error otherwise
+    """
+    def printInventory(self):
+        if self.inventory:
+            for id in range(len(self.inventory)):
+                print(f'({id + 1}.) {self[id]}')
+        else:
+            print("No inventory to display.")
+
+def main():
+    comp = Computer(
+        "2019 MacBook Pro",
+        "Intel",
+        256, 16,
+        "High Sierra", 2019, 1000)
+    comp2 = Computer(
+        "Mac Pro (Late 2013)",
+        "3.5 GHc 6-Core Intel Xeon E5",
+        1024, 64,
+        "macOS Big Sur", 2013, 1500
+    )
+    shop = ResaleShop()
+
+    shop.buy(comp)
+    shop.printInventory()
+    shop.buy(comp2)
+    shop.printInventory()
+
+    shop.sell(0)
+    shop.printInventory()
+
+    shop[0].refurbish()
+    shop.printInventory()
+
+main()
